33adb33f6d814e5640e6ca3a28671f51f784fcde
watare <aurelien.watare@gmail.com>
2022-10-25 22:04:31 +0200

    from param generate the full xml

--- VSA/ovs_conf/form/vmForm.py
+++ VSA/ovs_conf/form/vmForm.py
@@ -1,92 +1,25 @@
 
 from email.policy import default
 from faulthandler import disable
+from logging import PlaceHolder
+import resource
 from unicodedata import name
 from ovs_conf.models import DomainVm,MemoryVm
 from django import forms
 
-class DomainVmForm(forms.ModelForm):
-  class Meta:
-    model = DomainVm
-    fields = ('text_name','attr_type')
-    labels ={
-    'text_name' : 'name',
-    'attr_type' : 'type'        
-    }
-    widgets ={
-          'name' : forms.TextInput(attrs={'class':'form-control'}),
-          'type' : forms.TextInput(attrs={'class':'form-control'})     
-    }
-    
-class MemoryVmForm(forms.ModelForm):
-  class Meta:
-    model = MemoryVm
-    fields = ('text_memory',)
-    labels ={
-    'text_memory' : 'memory',        
-    }
-    widgets ={
-          'memory' : forms.NumberInput(attrs={'class':'form-control'}),
-    }
-# class BridgeForm(forms.ModelForm):
-#   select = forms.BooleanField(
-#       label='select',
-      
-#       widget=forms.CheckboxInput(attrs={'class':'form-check-label'})
-#       )
-#   class Meta:
-#     model = OvsBridge
-#     fields = ('name','rstp_enable','enable_ipv6')
-#     labels ={
-#     'name' : ''        
-#     }
-#     widgets ={
-#          'name' : forms.TextInput(attrs={'class':'form-control','placeholder':'Bridge name'}),
-#          'rstp_enable' : forms.CheckboxInput(attrs={'class':'form-check-label'}),
-#          'enable_ipv6' : forms.CheckboxInput(attrs={'class':'form-check-label'})
-#     }
-     
-# class BridgeFormRo(forms.ModelForm):
-   
-#    class Meta:
-#      model = OvsBridge
-#      fields = ('name','rstp_enable','enable_ipv6')
-#      labels ={
-#       'name' : ''        
-#      }
-#      widgets ={
-#          'name' : forms.TextInput(attrs={'class':'form-control','readonly':'readonly'}),
-#          'rstp_enable' : forms.CheckboxInput(attrs={'class':'form-check-label'}),
-#          'enable_ipv6' : forms.CheckboxInput(attrs={'class':'form-check-label'})
-#      }     
-     
-     
-# class PortForm(forms.ModelForm):
-#    class Meta:
-#     model = Port
-#     fields = {'bridge','name','type','interface','tag','vlan_mode','mac','remote_ip','remote_port','key','active'}
-#     labels ={
-#       'name' : '',
-#       'type' : '',
-#       'interface': '',
-#       'tag': '',
-#       'vlan_mode' : '',
-#       'mac' : '',
-#       'remote_ip':'',
-#       'remote_port':'',
-#       'key':''
-       
-#     }
-#     widgets ={
-#          'name' : forms.TextInput(attrs={'class':'form-control'}),
-#          'type' : forms.Select(attrs={'class': 'form-control selectpicker'}),
-#          'interface' : forms.TextInput(attrs={'class':'form-control'}),
-#          'tag' : forms.TextInput(attrs={'class':'form-control'}),
-#          'vlan_mode' : forms.Select(attrs={'class': 'form-control selectpicker'}),
-#          'mac' : forms.TextInput(attrs={'class':'form-control'}),
-#          'remote_ip' : forms.TextInput(attrs={'class':'form-control'}),
-#          'remote_port' : forms.TextInput(attrs={'class':'form-control'}),
-#          'key' : forms.TextInput(attrs={'class':'form-control'}),
-
-#      }
-    
+class VmForm(forms.Form):
+  name = forms.CharField(label='',widget=forms.TextInput(attrs={'class':'form-check-label' ,'placeholder':'name'})
+    )
+  memory = forms.IntegerField(label='',widget=forms.NumberInput(attrs={'class':'form-check-label' ,'placeholder':'memory(Kib)'})
+    )
+  vcpu = forms.IntegerField(label='',widget=forms.NumberInput(attrs={'class':'form-check-label' ,'placeholder':'vcpu'})
+    )
+  
+  PARTITIONS = (
+    ("/machine","machine"),
+    ("/rt","rt")
+  )
+  partition = forms.ChoiceField(choices=PARTITIONS,
+                               label='',
+                               widget=forms.RadioSelect(attrs={'class':'form-check-label' ,'placeholder':'partition'}))
+  
