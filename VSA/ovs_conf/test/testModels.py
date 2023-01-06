0230a85e2b3ddd48b7efcfca2e9176714e5df9a3
watare <aurelien.watare@gmail.com>
2023-01-06 18:28:54 +0100

    test : correct bug

--- VSA/ovs_conf/test/testModels.py
+++ VSA/ovs_conf/test/testModels.py
@@ -15,17 +15,15 @@ class OvsBridgeTestCase(TestCase):
         self.assertEqual(str(br0), 'br0')
         self.assertEqual(str(br1), 'br1')
 
-    def test_ordering(self):
-        bridges = OvsBridge.objects.all()
-        self.assertEqual(list(bridges), [
-            OvsBridge.objects.get(name='br0'),
-            OvsBridge.objects.get(name='br1'),
-        ])
+
 
 class PortTestCase(TestCase):
+       
     def setUp(self):
-        Port.objects.create(name='eth0', vlan_mode=VlanMode.TRUNK, vlan_tag=1)
-        Port.objects.create(name='eth1', vlan_mode=VlanMode.TRUNK, vlan_tag=2)
+        br0 = OvsBridge.objects.create(name='br0', rstp_enable=True, enable_ipv6=True)
+        br1 = OvsBridge.objects.create(name='br1', rstp_enable=False, enable_ipv6=True)
+        Port.objects.create(name='eth0',bridge=br1, vlan_mode=VlanMode.trunk, tag=1)
+        Port.objects.create(name='eth1',bridge=br0, vlan_mode=VlanMode.trunk, tag=2)
 
     def test_str_method(self):
         eth0 = Port.objects.get(name='eth0')
@@ -33,29 +31,27 @@ class PortTestCase(TestCase):
         self.assertEqual(str(eth0), 'eth0')
         self.assertEqual(str(eth1), 'eth1')
 
-    def test_ordering(self):
-        ports = Port.objects.all()
-        self.assertEqual(list(ports), [
-            Port.objects.get(name='eth0'),
-            Port.objects.get(name='eth1'),
-        ])
+
     def test_save_method(self):
         eth0 = Port.objects.get(name='eth0')
         eth0.save()
-        self.assertEqual(eth0.vlan_mode, VlanMode.TRUNK)
-        self.assertEqual(eth0.vlan_tag, 1)
+        self.assertEqual(eth0.vlan_mode, VlanMode.trunk)
+        self.assertEqual(eth0.tag, 1)
         
 class VlanModeTestCase(TestCase):
+        
     def test_str_method(self):
-        self.assertEqual(str(VlanMode.TRUNK), 'trunk')
-        self.assertEqual(str(VlanMode.ACCESS), 'access')
-        self.assertEqual(str(VlanMode.NATIVE_TAGGED), 'native-tagged')
-        self.assertEqual(str(VlanMode.NATIVE_UNTAGGED), 'native-untagged')
+        self.assertEqual(str(VlanMode.trunk), 'trunk')
+        self.assertEqual(str(VlanMode.access), 'access')
+        self.assertEqual(str(VlanMode.native_tagged), 'native-tagged')
+        self.assertEqual(str(VlanMode.native_untagged), 'native-untagged')
         
 class TrunkPortTestCase(TestCase):
     def setUp(self):
-        Port.objects.create(name='eth0', vlan_mode=VlanMode.TRUNK, vlan_tag=1)
-        Port.objects.create(name='eth1', vlan_mode=VlanMode.TRUNK, vlan_tag=2)
+        br0 = OvsBridge.objects.create(name='br0', rstp_enable=True, enable_ipv6=True)
+        br1 = OvsBridge.objects.create(name='br1', rstp_enable=False, enable_ipv6=True)
+        Port.objects.create(name='eth0',bridge=br1, vlan_mode=VlanMode.trunk, tag=1)
+        Port.objects.create(name='eth1',bridge=br0, vlan_mode=VlanMode.trunk, tag=2)
    
     def test_str_method(self):
         eth0 = Port.objects.get(name='eth0')
@@ -63,24 +59,22 @@ class TrunkPortTestCase(TestCase):
         self.assertEqual(str(eth0), 'eth0')
         self.assertEqual(str(eth1), 'eth1')
 
-    def test_ordering(self):
-        ports = Port.objects.all()
-        self.assertEqual(list(ports), [
-            Port.objects.get(name='eth0'),
-            Port.objects.get(name='eth1'),
-        ])
-     def save_method(self):
+
+    def save_method(self):
         eth0 = Port.objects.get(name='eth0')
         eth0.save()
-        self.assertEqual(eth0.vlan_mode, VlanMode.TRUNK)
-        self.assertEqual(eth0.vlan_tag, 1)
+        self.assertEqual(eth0.vlan_mode, VlanMode.trunk)
+        self.assertEqual(eth0.tag, 1)
 
         
 class OtherPortTestCase(TestCase):
+        
     def setUp(self):
-        Port.objects.create(name='eth0', vlan_mode=VlanMode.ACCESS, vlan_tag=1)
-        Port.objects.create(name='eth1', vlan_mode=VlanMode.NATIVE_TAGGED, vlan_tag=2)
-        Port.objects.create(name='eth2', vlan_mode=VlanMode.NATIVE_UNTAGGED, vlan_tag=3)
+        br0 = OvsBridge.objects.create(name='br0', rstp_enable=True, enable_ipv6=True)
+        br1 = OvsBridge.objects.create(name='br1', rstp_enable=False, enable_ipv6=True)
+        Port.objects.create(name='eth0',bridge=br1, vlan_mode=VlanMode.access, tag=1)
+        Port.objects.create(name='eth1',bridge=br0, vlan_mode=VlanMode.native_tagged, tag=2)
+        Port.objects.create(name='eth2',bridge=br1, vlan_mode=VlanMode.native_untagged, tag=3)
 
     def test_str_method(self):
         eth0 = Port.objects.get(name='eth0')
@@ -90,29 +84,22 @@ class OtherPortTestCase(TestCase):
         self.assertEqual(str(eth1), 'eth1')
         self.assertEqual(str(eth2), 'eth2')
 
-    def test_ordering(self):
-        ports = Port.objects.all()
-        self.assertEqual(list(ports), [
-            Port.objects.get(name='eth0'),
-            Port.objects.get(name='eth1'),
-            Port.objects.get(name='eth2'),
-        ])
         
     def save_method(self):
         eth0 = Port.objects.get(name='eth0')
         eth0.save()
-        self.assertEqual(eth0.vlan_mode, VlanMode.ACCESS)
-        self.assertEqual(eth0.vlan_tag, 1)
+        self.assertEqual(eth0.vlan_mode, VlanMode.access)
+        self.assertEqual(eth0.tag, 1)
         
         eth1 = Port.objects.get(name='eth1')
         eth1.save()
-        self.assertEqual(eth1.vlan_mode, VlanMode.NATIVE_TAGGED)
-        self.assertEqual(eth1.vlan_tag, 2)
+        self.assertEqual(eth1.vlan_mode, VlanMode.native_tagged)
+        self.assertEqual(eth1.tag, 2)
         
         eth2 = Port.objects.get(name='eth2')
         eth2.save()
-        self.assertEqual(eth2.vlan_mode, VlanMode.NATIVE_UNTAGGED)
-        self.assertEqual(eth2.vlan_tag, 3)
+        self.assertEqual(eth2.vlan_mode, VlanMode.native_untagged)
+        self.assertEqual(eth2.tag, 3)
         
 
 
